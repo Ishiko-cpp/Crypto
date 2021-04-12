ee868a7ced9466594aa3503ec64c78ba2d37384b
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-04-13 00:17:22 +0100

    Add SHA512 hash (#4)

---
+++ Tests/Source/main.cpp
@@ -0,0 +1,23 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/Ishiko-cpp/Crypto/blob/master/LICENSE.txt
+*/
+
+#include "SHA512HashTests.h"
+#include <Ishiko/TestFramework/TestFrameworkCore.h>
+
+using namespace Ishiko::Tests;
+
+int main(int argc, char* argv[])
+{
+    TestHarness theTestHarness("IshikoCrypto");
+
+    theTestHarness.environment().setTestDataDirectory("../../TestData");
+    theTestHarness.environment().setTestOutputDirectory("../../TestOutput");
+
+    TestSequence& theTests = theTestHarness.tests();
+    theTests.append<SHA512HashTests>();
+
+    return theTestHarness.run();
+}
